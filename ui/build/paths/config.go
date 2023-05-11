93359e2d27259860892fe0a50c69347952561433
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-11 20:43:05 +0800

    build: whitelist aarch64-linux-gnu-ld
    Disallowed PATH tool "aarch64-linux-gnu-ld" used: []string{
    out/.path/aarch64-linux-gnu-ld", "-EL", "-z", "relro", "--hash-style=gnu
    ", "--eh-frame-hdr", "-m", "aarch64linux", "-static", "-o", "/dev/null", "crt1.o
    ", "crti.o", "crtbeginT.o",
    
    Change-Id: Icee30263af4cc981c8724f8e59897ceb6c47f179
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -74,6 +74,7 @@ func GetConfig(name string) PathConfig {
 }
 
 var Configuration = map[string]PathConfig{
+	"aarch64-linux-gnu-ld":    Allowed,
 	"bash":    Allowed,
 	"brotli":  Allowed,
 	"ccache":  Allowed,
