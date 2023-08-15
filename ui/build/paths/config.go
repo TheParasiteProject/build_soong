55ef3a40b76e7c4f80d12d9871c47300317161b2
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-16 07:49:33 +0800

    Whitelist pahole tool
    * used by gs kernels
    
    Change-Id: I579e4fc0c9bf2a9b2a87a7ca391c63d28d54ec55
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -112,6 +112,7 @@ var Configuration = map[string]PathConfig{
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"patch":   Allowed,
+	"pahole":   Allowed,
 	"pkg-config": Allowed,
 	"pstree":  Allowed,
 	"python3": Allowed,
