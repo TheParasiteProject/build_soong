a58b2e0a4c288766bf6ebadd43b25fbb9241e12e
someone5678 <someone5678@users.noreply.github.com>
2024-01-17 02:36:10 +0900

    soong: Don't add lineage-sdk resources to framework-res
    * What we want here is not adding lineage-sdk resources directly into frameworks
    
    Change-Id: I41c25e52d611de562466ad7fc929b9e153ac8cb2

--- java/aar.go
+++ java/aar.go
@@ -402,7 +402,7 @@ func (a *aapt) deps(ctx android.BottomUpMutatorContext, sdkDep sdkDep) {
 	if sdkDep.frameworkResModule != "" {
 		ctx.AddVariationDependencies(nil, frameworkResTag, sdkDep.frameworkResModule)
 	}
-	if sdkDep.lineageResModule != "" {
+	if sdkDep.lineageResModule != "" && ctx.ModuleName() != "framework-res" {
 		ctx.AddVariationDependencies(nil, lineageResTag, sdkDep.lineageResModule)
 	}
 }
@@ -1258,7 +1258,7 @@ func (a *AARImport) DepsMutator(ctx android.BottomUpMutatorContext) {
 		if sdkDep.useModule && sdkDep.frameworkResModule != "" {
 			ctx.AddVariationDependencies(nil, frameworkResTag, sdkDep.frameworkResModule)
 		}
-		if sdkDep.useModule && sdkDep.lineageResModule != "" {
+		if sdkDep.useModule && sdkDep.lineageResModule != "" && ctx.ModuleName() != "framework-res" {
 			ctx.AddVariationDependencies(nil, lineageResTag, sdkDep.lineageResModule)
 		}
 	}
