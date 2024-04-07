4f8a035f1ab1a74040052ec4e572f917c0be3597
someone5678 <someone5678@users.noreply.github.com>
2024-04-07 17:07:33 +0900

    soong: Forbidden pkg-config again
    * Fixes kernel build failure in Arch Linux due to
      missing header file error even if it exists
    
    Log:
    /home/someone5678/android/TheParasiteProject/kernel/sony/sm8550/scripts/dtc/yamltree.c:9:10: fatal error: 'yaml.h' file not found
             ^~~~~~~~
    
    Change-Id: I5a1fab8f1d741eb2bcf7b1ca0279816e531a052d

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -123,7 +123,6 @@ var Configuration = map[string]PathConfig{
 	"pahole":         Allowed,
 	"patch":          Allowed,
 	"perl":           Allowed,
-	"pkg-config":     Allowed,
 	"pstree":         Allowed,
 	"python3":        Allowed,
 	"python3.6":      Allowed,
@@ -154,6 +153,7 @@ var Configuration = map[string]PathConfig{
 	"ld":         Forbidden,
 	"ld.bfd":     Forbidden,
 	"ld.gold":    Forbidden,
+	"pkg-config": Forbidden,
 
 	// These are toybox tools that only work on Linux.
 	"pgrep": LinuxOnlyPrebuilt,
