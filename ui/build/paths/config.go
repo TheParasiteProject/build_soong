8674e72560aa43b06809f2b57fd726a7ecb3a36a
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-20 18:12:29 +0900

    soong: Whitelist jq
    Change-Id: I885eca97ed6d625edf411126ac4e81a5a6b4da6b

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -140,6 +140,7 @@ var Configuration = map[string]PathConfig{
 	"repo":        Allowed,
 	"tar":         Allowed,
 	"yes":         Allowed,
+	"jq":          Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
