0235be1e0c145d4161f050ff633c642225d04369
Krzysztof Kosiński <krzysio@google.com>
2025-03-27 01:55:48 +0000

    Put AArch64 branch protection flags in a feature.
    I need to add ARMv8.5-A and ARMv8.7-A and the duplication
    was getting out of hand. Instead of specifying these as arch
    variant flags, specify them as arch feature flags for a new
    feature called "branchprot" that is enabled on the relevant
    variants.
    
    This also fixes an inconsitency where armv8-a-branchprot
    enabled branch protection, but didn't disable the stack
    protector, which appears to have been an omission.
    
    Bug: 364383722
    Test: inspect compilation flags in a local build, presubmit
    Flag: EXEMPT infeasible
    Change-Id: Ib5f89b5f0f7a597118edaf2b7a70dbad515616f5

--- android/arch_list.go
+++ android/arch_list.go
@@ -115,6 +115,7 @@ var cpuVariants = map[ArchType][]string{
 
 var archFeatures = map[ArchType][]string{
 	Arm64: {
+		"branchprot",
 		"dotprod",
 	},
 	X86: {
@@ -144,19 +145,26 @@ var archFeatures = map[ArchType][]string{
 
 var androidArchFeatureMap = map[ArchType]map[string][]string{
 	Arm64: {
+		"armv8-a-branchprot": {
+			"branchprot",
+		},
 		"armv8-2a-dotprod": {
 			"dotprod",
 		},
 		"armv9-a": {
+			"branchprot",
 			"dotprod",
 		},
 		"armv9-2a": {
+			"branchprot",
 			"dotprod",
 		},
 		"armv9-3a": {
+			"branchprot",
 			"dotprod",
 		},
 		"armv9-4a": {
+			"branchprot",
 			"dotprod",
 		},
 	},
