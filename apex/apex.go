e6a0c71e0097e86d1ace7f32c1eaf457e0491ed2
Jooyung Han <jooyung@google.com>
2025-04-01 16:03:19 +0900

    apex.compressible should be used with system/updatable
    It doesn't make sense to compress non-updatable apexes. It will use more
    storage and processing time.
    
    Bug: 407670345
    Flag: EXEMPT build-time checks
    Test: m --no-skip-soong-tests
    Change-Id: If12875239ee3fc8932d6112c6fc27966d0230336

--- apex/apex.go
+++ apex/apex.go
@@ -1640,6 +1640,15 @@ func (f fsType) string() string {
 }
 
 func (a *apexBundle) setCompression(ctx android.ModuleContext) {
+	if a.isCompressable() {
+		if !a.Updatable() {
+			ctx.PropertyErrorf("compressible", "do not compress non-updatable APEX")
+		}
+		if a.PartitionTag(ctx.DeviceConfig()) != "system" {
+			ctx.PropertyErrorf("compressible", "do not compress non-system APEX")
+		}
+	}
+
 	if a.testOnlyShouldForceCompression() {
 		a.isCompressed = true
 	} else {
