1fce9f9bb82edbb28b7da8e28674d5f24aca3012
Dave Mankoff <mankoff@google.com>
2025-05-05 09:43:24 -0700

    Revert "Revert "Initial pass at a semi-functional Kotlin Compile..."
    Revert submission 33373055-revert-32464406-b392906864-kotlin-incremental-RKSJYQSKFU
    
    Reason for revert: Restoring so I can fix it.
    
    Reverted changes: /q/submissionid:33373055-revert-32464406-b392906864-kotlin-incremental-RKSJYQSKFU
    
    Bug: 392906864
    Test: m && m
    Change-Id: I4249ad599de7b5ca28f723f640473ec4f911fd16

--- cmd/kotlinc_incremental/src/com/android/kotlin/compiler/client/Main.kt
+++ cmd/kotlinc_incremental/src/com/android/kotlin/compiler/client/Main.kt
@@ -16,7 +16,17 @@
 
 package com.android.kotlin.compiler.client
 
+import java.io.File
+import java.net.URLClassLoader
+import java.util.UUID
 import kotlin.system.exitProcess
+import org.jetbrains.kotlin.buildtools.api.CompilationService
+import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
+import org.jetbrains.kotlin.buildtools.api.ProjectId
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.jvm.ClassSnapshotGranularity
+import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathEntrySnapshot
+import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathSnapshotBasedIncrementalCompilationApproachParameters
 
 private val ARGUMENT_PARSERS =
     listOf(
@@ -32,6 +42,7 @@ private val ARGUMENT_PARSERS =
         RunFilesArgument(),
         RootDirArgument(),
         WorkingDirArgument(),
+        XBuildFileArgument(),
         SourcesArgument(), // must come last
     )
 
@@ -43,29 +54,39 @@ fun main(args: Array<String>) {
         exitProcess(-1)
     }
 
-    println("compiling")
+    if (opts.sources.isEmpty() && (opts.buildFile == null || opts.buildFileSources.isEmpty())) {
+        println("No sources or build file specified. Exiting.")
+        exitProcess(0)
+    }
+
+    BTACompilation(opts)
 }
 
 fun parseArgs(args: Array<String>, opts: Options): Boolean {
+    var matched = false
     var hasError = false
     var showHelp = args.isEmpty()
     val iter = args.iterator()
     while (iter.hasNext()) {
         val arg = iter.next()
+        matched = false
         for (parser in ARGUMENT_PARSERS) {
             if (parser.matches(arg)) {
+                matched = true
                 if (parser is HelpArgument) {
                     showHelp = true
                 }
                 parser.parse(arg, iter, opts)
                 if (parser.error != null) {
-                    println("The error: " + parser.error)
                     hasError = true
                     System.err.println(parser.error)
                     System.err.println()
                 }
                 break
             }
+        }
+        if (!matched) {
+            opts.passThroughArgs.add(arg.substring(0))
         }
     }
 
@@ -112,4 +133,118 @@ fun showArgumentHelp() {
             }
         }
     }
+}
+
+fun BTACompilation(opts: Options) {
+    val kotlincArgs = mutableListOf<String>()
+    if (opts.buildFile != null) {
+        if (opts.buildFileModuleName != null) {
+            kotlincArgs.add("-module-name")
+            kotlincArgs.add(opts.buildFileModuleName!!)
+        }
+    }
+    kotlincArgs.add("-d=${opts.outputDir.absolutePath}")
+    kotlincArgs.addAll(opts.passThroughArgs)
+    kotlincArgs.addAll(opts.sources)
+    kotlincArgs.addAll(opts.buildFileJavaSources)
+    doBtaCompilation(
+        opts.sources + opts.buildFileSources,
+        opts.classPath + opts.buildFileClassPaths,
+        opts.workingDir,
+        kotlincArgs,
+        opts.jvmArgs,
+        Logger(),
+    )
+}
+
+@OptIn(ExperimentalBuildToolsApi::class)
+fun doBtaCompilation(
+    sources: List<String>,
+    classPath: List<String>,
+    workingDirectory: File,
+    args: List<String>,
+    jvmArgs: List<String>,
+    logger: Logger,
+) {
+    val loader =
+        URLClassLoader(
+            classPath.map { File(it).toURI().toURL() }.toTypedArray() +
+                // TODO: don't hardcode this path.
+                arrayOf(
+                        "out/soong/.intermediates/external/kotlinc/kotlin-build-tools-impl/" +
+                            "linux_glibc_common/local-combined/kotlin-build-tools-impl.jar"
+                        // TODO: this root path also won't work on other's computers!
+                    )
+                    .map { File("/src/android/aosp/$it").toURI().toURL() } +
+
+                // Need to include this code's own jar in the classpath.
+                arrayOf(Options::class.java.protectionDomain?.codeSource?.location)
+        )
+
+    val service = CompilationService.loadImplementation(loader)
+    val executionConfig = service.makeCompilerExecutionStrategyConfiguration()
+    // TODO: investigate using the daemon.
+    // Right now, it hangs (https://youtrack.jetbrains.com/issue/KT-75142/)
+    // executionConfig.useDaemonStrategy(jvmArgs)
+    executionConfig.useInProcessStrategy()
+    val compilationConfig = service.makeJvmCompilationConfiguration()
+
+    val cpsnapshotParameters =
+        getClasspathSnapshotParameters(
+            workingDirectory,
+            classPath,
+            service::calculateClasspathSnapshot,
+        )
+
+    val sourcesChanges =
+        SourcesChanges.Known(
+            modifiedFiles = listOf(sources.first()).map { File(it) },
+            removedFiles = emptyList(),
+        )
+    val incJvmCompilationConfig =
+        compilationConfig.makeClasspathSnapshotBasedIncrementalCompilationConfiguration()
+    incJvmCompilationConfig.assureNoClasspathSnapshotsChanges(true)
+    compilationConfig.useIncrementalCompilation(
+        workingDirectory,
+        sourcesChanges,
+        cpsnapshotParameters,
+        incJvmCompilationConfig,
+    )
+    compilationConfig.useLogger(logger)
+
+    val pid = ProjectId.ProjectUUID(UUID.randomUUID())
+    val mArgs = args.toMutableList()
+    mArgs.add("-cp")
+    mArgs.add(classPath.joinToString(":"))
+    service.compileJvm(pid, executionConfig, compilationConfig, sources.map { File(it) }, mArgs)
+}
+
+@OptIn(ExperimentalBuildToolsApi::class)
+fun getClasspathSnapshotParameters(
+    workingDirectory: File,
+    classPath: List<String>,
+    calculateClasspathSnapshot: (File, ClassSnapshotGranularity) -> ClasspathEntrySnapshot,
+): ClasspathSnapshotBasedIncrementalCompilationApproachParameters {
+    val cps = File(workingDirectory.parentFile, "shrunk-classpath-snapshot.bin")
+    val cpsFiles =
+        classPath.map {
+            val cpFile = File(it)
+            // TODO: Consider CLASS_LEVEL snapshots of things that change infrequently.
+            // CLASS_MEMBER_LEVEL
+            // of everything else.
+            val snName = cpFile.name.replace(".", "_") + "-snapshot.bin"
+            val snf = File(workingDirectory.parentFile, snName)
+            // TODO: we need to delete/regenerate the snf if the jar has changed.
+            if (!snf.exists()) {
+                val sn =
+                    calculateClasspathSnapshot(cpFile, ClassSnapshotGranularity.CLASS_MEMBER_LEVEL)
+                sn.saveSnapshot(snf)
+            }
+            snf
+        }
+
+    return ClasspathSnapshotBasedIncrementalCompilationApproachParameters(
+        newClasspathSnapshotFiles = cpsFiles,
+        shrunkClasspathSnapshot = cps,
+    )
 }
