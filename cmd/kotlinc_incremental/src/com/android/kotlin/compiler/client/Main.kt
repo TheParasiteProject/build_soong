b2126ee3b926dc8292ba3a7bc1391b35b7058e82
Dave Mankoff <mankoff@google.com>
2025-05-05 09:43:24 -0700

    Revert^2 "Add command line arguments to the client."
    1649e912b090fd986c0cc4649bf98a987488a863
    
    Bug: 392906864
    Test: m && m
    Change-Id: If78c62fc756a44899fbe6cd18f187e581c98d494

--- cmd/kotlinc_incremental/src/com/android/kotlin/compiler/client/Main.kt
+++ cmd/kotlinc_incremental/src/com/android/kotlin/compiler/client/Main.kt
@@ -16,6 +16,100 @@
 
 package com.android.kotlin.compiler.client
 
+import kotlin.system.exitProcess
+
+private val ARGUMENT_PARSERS =
+    listOf(
+        BuildDirArgument(),
+        BuildFileArgument(),
+        BuildHistoryFileArgument(),
+        ClassPathArgument(),
+        HelpArgument(),
+        JvmArgument(),
+        LogDirArgument(),
+        OutputDirArgument(),
+        PluginArgument(),
+        RunFilesArgument(),
+        RootDirArgument(),
+        WorkingDirArgument(),
+        SourcesArgument(), // must come last
+    )
+
 fun main(args: Array<String>) {
-  println("compiling")
+    val opts = Options()
+    ARGUMENT_PARSERS.forEach { it.setupDefault(opts) }
+
+    if (!parseArgs(args, opts)) {
+        exitProcess(-1)
+    }
+
+    println("compiling")
+}
+
+fun parseArgs(args: Array<String>, opts: Options): Boolean {
+    var hasError = false
+    var showHelp = args.isEmpty()
+    val iter = args.iterator()
+    while (iter.hasNext()) {
+        val arg = iter.next()
+        for (parser in ARGUMENT_PARSERS) {
+            if (parser.matches(arg)) {
+                if (parser is HelpArgument) {
+                    showHelp = true
+                }
+                parser.parse(arg, iter, opts)
+                if (parser.error != null) {
+                    println("The error: " + parser.error)
+                    hasError = true
+                    System.err.println(parser.error)
+                    System.err.println()
+                }
+                break
+            }
+        }
+    }
+
+    if (showHelp) {
+        showArgumentHelp()
+    }
+
+    return !hasError
+}
+
+fun showArgumentHelp() {
+    var longest = -1
+    val padding = 5
+
+    println(
+        "Usage: kotlin-incremental-client <-root-dir>=<dir> [options] [kotlinc options] " +
+            "[-- <source files>]"
+    )
+    println()
+    for (parser in ARGUMENT_PARSERS) {
+        if (parser.argumentName.length > longest) {
+            longest = parser.argumentName.length
+        }
+    }
+
+    val indent = " ".repeat(longest + padding)
+    for (parser in ARGUMENT_PARSERS) {
+        print(("-" + parser.argumentName).padEnd(longest + padding))
+        var first = true
+        parser.helpText.lines().forEach {
+            if (first) {
+                println(it)
+                first = false
+            } else {
+                println(indent + it)
+            }
+        }
+        if (parser.default != null) {
+            print(indent + "[Default: ")
+            if (parser.default is String) {
+                println("\"${parser.default}\"]")
+            } else {
+                println("${parser.default}]")
+            }
+        }
+    }
 }
