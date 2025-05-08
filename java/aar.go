9d0f52745f61273a50cb1a4c60847ad19f29cf1f
Sean Hua <huasea@google.com>
2025-05-08 21:54:42 +0000

    Write asset and resource dirs to jdeps
    Aapt-reliant modules should write their asset and resource dirs
    in jdeps so IDE consumers are aware of their locations.
    
    Bug: 416279279
    Test: "ANGLE_library": {
      "dependencies": [
        ...
      ],
      "jars": [
        ...
      ],
      "path": [
        "external/angle"
      ],
      "static_libs": [
        "androidx.preference_preference"
      ],
      "asset_dirs": [
        "external/angle/src/android_system_settings/assets"
      ],
      "resource_dirs": [
        "external/angle/src/android_system_settings/res"
      ]
    }
    
    Change-Id: I2885d931cf9111ec3253bc0aed0326fdbdc1c3eb

--- java/aar.go
+++ java/aar.go
@@ -147,6 +147,10 @@ type aapt struct {
 	manifestValues struct {
 		applicationId string
 	}
+
+	// Fields written to jdeps
+	assetDirs    android.Paths
+	resourceDirs android.Paths
 }
 
 type split struct {
@@ -282,7 +286,10 @@ func (a *aapt) aapt2Flags(ctx android.ModuleContext, sdkContext android.SdkConte
 	} else {
 		assetDirs = android.PathsWithOptionalDefaultForModuleSrc(ctx, a.aaptProperties.Asset_dirs, "assets")
 	}
+	a.assetDirs = assetDirs
+
 	resourceDirs := android.PathsWithOptionalDefaultForModuleSrc(ctx, a.aaptProperties.Resource_dirs.GetOrDefault(ctx, nil), "res")
+	a.resourceDirs = resourceDirs
 	resourceZips := android.PathsForModuleSrc(ctx, a.aaptProperties.Resource_zips)
 
 	// Glob directories into lists of paths
@@ -1117,6 +1124,8 @@ func (a *aapt) IDEInfo(ctx android.BaseModuleContext, dpInfo *android.IdeInfo) {
 	if a.rJar != nil {
 		dpInfo.Jars = append(dpInfo.Jars, a.rJar.String())
 	}
+	dpInfo.Asset_dirs = append(dpInfo.Asset_dirs, a.assetDirs.Strings()...)
+	dpInfo.Resource_dirs = append(dpInfo.Resource_dirs, a.resourceDirs.Strings()...)
 }
 
 // android_library builds and links sources into a `.jar` file for the device along with Android resources.
