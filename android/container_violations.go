b1beada5a94e65cb5096db6b2e54653339e66848
Rajeev Kumar <rajekumar@google.com>
2025-05-29 07:40:09 -0700

    Update allowlist to add CtsSyncManagerTestCases.
    This change is necessary since we are refactoring an existing test class from CtsContentTestCases, which is already part of allowlist.
    
    Fixes: 421154651
    Test: atest CtsSyncManagerTestCases
    Flag: TEST_ONLY
    Change-Id: I09b0e3d12fd94c3be19d9a91c119a488811a0baf

--- android/container_violations.go
+++ android/container_violations.go
@@ -812,6 +812,10 @@ var ContainerDependencyViolationAllowlist = map[string][]string{
 		"framework", // cts -> unstable
 	},
 
+        "CtsSyncManagerTestCases": {
+                "framework", // cts -> unstable
+        },
+
 	"CtsSystemUiTestCases": {
 		"framework", // cts -> unstable
 	},
