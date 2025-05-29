0d5b287d1c850ff05a803283a1ab22c9ad7b00a8
Cole Faust <colefaust@google.com>
2025-05-29 10:21:29 -0700

    Revert "Dist apis used by apexes in soong"
    This reverts commit 9783025c82644a4a29498cfc585e18606e451c0c.
    
    Reason for revert: b/421107041
    
    Bug: 413145177
    Change-Id: If06f4fb76151f4749e78f9ebc2fecad6901d59cf

--- apex/apex.go
+++ apex/apex.go
@@ -541,6 +541,11 @@ type apexBundle struct {
 
 	isCompressed bool
 
+	// Path of API coverage generate file
+	nativeApisUsedByModuleFile   android.ModuleOutPath
+	nativeApisBackedByModuleFile android.ModuleOutPath
+	javaApisUsedByModuleFile     android.ModuleOutPath
+
 	aconfigFiles []android.Path
 
 	// Required modules, filled out during GenerateAndroidBuildActions and used in AndroidMk
