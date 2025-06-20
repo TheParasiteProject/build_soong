6b408727e672fc01f042cb060d4d02e0bb5140d4
Ivan Lozano <ivanlozano@google.com>
2025-06-20 18:44:52 +0000

    rust: Fix RBE OUT_DIR handling for generated srcs
    In the include! macro (used to pull in generated source code), Rust
    expects a source-file-relative path for OUT_DIR. This doesn't work
    for Soong, so we get around this by using an absolute path and setting
    the OUT_DIR environment variable to that using $$PWD.
    
    However, this breaks RBE since $$PWD is calculated before being handed
    over to the remote build. Thus, the remote build servers OUT_DIR points
    to the local OUT_DIR rather than its own.
    
    To get around this, we use a wrapper around rustc and clippy invocations
    that appends `pwd` at compile-time (rather than at rule generation
    time). This resolves the RBE breakage for generated sources.
    
    Bug: 407072297
    Test: RBE_RUST_EXEC_STRATEGY=remote m <module with generated srcs>
    
    Change-Id: I733dbb3348b29e90204c01580c855a4ef6253da7

---
+++ scripts/rustc_wrapper.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A wrapper around rustc/clippy to set up the correct absolute path for OUT_DIR.
+# This has to be done on invocation rather than precalucalted in Soong to
+# ensure that RBE calculates its own absolute path rather than inheriting the
+# local host absolute path.
+#
+# The first argument should be the path to rustc/clippy
+
+# Check if $SOONG_RUST_GEN_DIR is set, otherwise don't do anything.
+if [ -v SOONG_RUST_GEN_DIR ]; then
+  if [[ $SOONG_RUST_GEN_DIR =~ ^/ ]]; then
+    export OUT_DIR=$SOONG_RUST_GEN_DIR
+  else
+    export OUT_DIR=`pwd`/$SOONG_RUST_GEN_DIR
+  fi
+fi
+
+exec_path=$1
+shift
+exec $exec_path "$@"
