5e4055368b89689eb784c10b8bbdaaeb205fd536
Jim Tang <jimtang@google.com>
2025-07-22 18:30:18 +0800

    Enrich features of compatibility_test_suite_package
    This commit brings the following properties:
    * `no_dist`: (boolean) Prevents the test suite from being included
      in dist files.
    * `test_suite_name`: (string) Allows the test suite name to differ
      from the module name, which is useful for avoiding naming conflicts
      during post-processing.
    
    Flag: EXEMPT bugfix
    Bug: 431637826
    Test: m autorepro dist
    Test: m autorepro
    Change-Id: Ie7e926f0aac6fe0c61bb9457eef6ea77aea35f1d

--- test_suites/test_suites.go
+++ test_suites/test_suites.go
@@ -699,7 +699,10 @@ func buildCompatibilitySuitePackage(
 	builder.Build("compatibility_zip_"+testSuiteName, fmt.Sprintf("Compatibility test suite zip %q", testSuiteName))
 
 	ctx.Phony(testSuiteName, out)
-	ctx.DistForGoal(testSuiteName, out)
+	// TODO: Enable NoDist by default for all compatibility test suites.
+	if !suite.NoDist {
+		ctx.DistForGoal(testSuiteName, out)
+	}
 
 	if suite.BuildMetadata {
 		metadata_rule := android.NewRuleBuilder(pctx, ctx)
@@ -714,7 +717,9 @@ func buildCompatibilitySuitePackage(
 		metadata_rule.Build("compatibility_metadata_"+testSuiteName, fmt.Sprintf("Compatibility test suite metadata file %q", testSuiteName))
 
 		ctx.Phony(testSuiteName, compatibility_files_metadata)
-		ctx.DistForGoal(testSuiteName, compatibility_files_metadata)
+		if !suite.NoDist {
+			ctx.DistForGoal(testSuiteName, compatibility_files_metadata)
+		}
 	}
 }
 
@@ -756,6 +761,11 @@ type compatibilityTestSuitePackageProperties struct {
 	Dynamic_config   *string `android:"path"`
 	Host_shared_libs []string
 	Build_metadata   *bool
+	// If true, the test suite will not be included in the dist-for-goal method.
+	No_dist *bool
+	// If set, this will override the name property used in the zip file. This is useful when the test suite
+	// requires post-processing, so the module name does not conflict with the original test suite name.
+	Test_suite_name *string `json:"test_suite_name"`
 }
 
 type compatibilityTestSuitePackage struct {
@@ -771,6 +781,7 @@ type compatibilitySuitePackageInfo struct {
 	ToolNoticeInfo android.NoticeModuleInfos
 	HostSharedLibs android.Paths
 	BuildMetadata  bool
+	NoDist         bool
 }
 
 var compatibilitySuitePackageProvider = blueprint.NewProvider[compatibilitySuitePackageInfo]()
@@ -817,8 +828,13 @@ func (m *compatibilityTestSuitePackage) DepsMutator(ctx android.BottomUpMutatorC
 }
 
 func (m *compatibilityTestSuitePackage) GenerateAndroidBuildActions(ctx android.ModuleContext) {
-	if matched, err := regexp.MatchString("[a-zA-Z0-9_-]+", m.Name()); err != nil || !matched {
-		ctx.ModuleErrorf("Invalid test suite name, must match [a-zA-Z0-9_-]+, got %q", m.Name())
+	suiteName := m.Name()
+	if m.properties.Test_suite_name != nil && *m.properties.Test_suite_name != "" {
+		suiteName = *m.properties.Test_suite_name
+	}
+
+	if matched, err := regexp.MatchString("[a-zA-Z0-9_-]+", suiteName); err != nil || !matched {
+		ctx.ModuleErrorf("Invalid test suite name, must match [a-zA-Z0-9_-]+, got %q", suiteName)
 		return
 	}
 
@@ -877,15 +893,18 @@ func (m *compatibilityTestSuitePackage) GenerateAndroidBuildActions(ctx android.
 	}
 
 	android.SetProvider(ctx, compatibilitySuitePackageProvider, compatibilitySuitePackageInfo{
-		Name:           m.Name(),
+		Name:           suiteName,
 		Readme:         readme,
 		DynamicConfig:  dynamicConfig,
 		ToolFiles:      toolFiles,
 		ToolNoticeInfo: toolNoticeinfo,
 		HostSharedLibs: hostSharedLibs,
 		BuildMetadata:  proptools.Bool(m.properties.Build_metadata),
+		NoDist:         proptools.Bool(m.properties.No_dist),
 	})
-	ctx.SetOutputFiles(android.Paths{android.PathForHostInstall(ctx, m.Name(), fmt.Sprintf("android-%s.zip", m.Name()))}, "")
+
+	// Make compatibility_test_suite_package a SourceFileProducer so that it can be used by other modules.
+	ctx.SetOutputFiles(android.Paths{android.PathForHostInstall(ctx, suiteName, fmt.Sprintf("android-%s.zip", suiteName))}, "")
 }
 
 func testSuitePackageFactory() android.Module {
